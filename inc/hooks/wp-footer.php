84634a30f7e3c0e6ce9498e6d60342b2ab7196c2
Pea <pea@misfist.com>
2024-08-30 15:47:54 -0400

    Add back to top scroller

---
+++ inc/hooks/wp-footer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Filter wp_footer
+ *
+ * @package ttt
+ */
+
+namespace Quincy\ttt;
+
+/**
+ * Add back to top link to footer
+ *
+ * @return void
+ */
+function wp_footer() : void {
+	?>
+	<a href="#top"><?php esc_html_e( 'Jump to top of page', 'ttt' ); ?></a>
+	<?php
+}
+add_action( 'wp_footer', __NAMESPACE__ . '\wp_footer' );
