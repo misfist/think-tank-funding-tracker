74d45150708748f9f287df9eb32956f9f2500b5a
Pea <pea@misfist.com>
2024-10-01 01:19:07 -0400

    Move search bar to pattern

---
+++ patterns/content-footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Content Footer
+ * Slug: ttt/content-footer
+ * Categories: hidden
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"metadata":{"name":"Content Footer"},"className":"content-footer","layout":{"type":"constrained"}} -->
+<div class="wp-block-group content-footer">
+	<!-- wp:group {"metadata":{"name":"Content Footer Wrapper"}} -->
+	<div class="wp-block-group">
+		<!-- wp:pattern {"slug":"ttt/search-form"} /-->
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
