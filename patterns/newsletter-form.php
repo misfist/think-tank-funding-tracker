42cea07d83b4268af359d86b399610377fe515c8
Pea <pea@misfist.com>
2024-10-01 01:18:50 -0400

    Add newsletter placeholder

---
+++ patterns/newsletter-form.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Newsletter Form
+ * Slug: ttt/newsletter-form
+ * Inserter: no
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Newsletter"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"backgroundColor":"accent-10","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-accent-10-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
+	<!-- wp:paragraph -->
+	<p>Newsletter form here.</p>
+	<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
