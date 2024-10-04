b3d15a28f2e5066b7cdb689d2042a6e12e940493
Pea <pea@misfist.com>
2024-10-03 21:02:27 -0400

    Add body classes
    Add `is-limited` and `is-transparent` classes to body

---
+++ inc/hooks/body-classes.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Modify Body Classes.
+ *
+ * @package ttt
+ */
+
+namespace Quincy\ttft;
+
+/**
+ * Add body classes
+ * 
+ * @link https://developer.wordpress.org/reference/hooks/body_class/
+ *
+ * @param  array $classes
+ * @return array
+ */
+function body_classes( $classes ): array {
+	if ( is_singular( 'think_tank' ) ) {
+		$is_transparent = is_transparent();
+		if(  $is_transparent ) {
+			$classes[] = 'is-transparent';
+		}
+		$is_limited = is_limited();
+		if(  $is_limited ) {
+			$classes[] = 'is-limited';
+		}
+	}
+	return $classes;
+}
+add_filter( 'body_class', __NAMESPACE__ . '\body_classes' );
