9394fbf418f0296a2e65e94e6aaf7dd88c0ae6d0
Pea <pea@misfist.com>
2024-10-04 23:06:58 -0400

    Single donor block
    Update to add correct donor total and use text from settings

--- patterns/data-table-donor.php
+++ patterns/data-table-donor.php
@@ -15,21 +15,8 @@ use function Quincy\ttft\get_single_donor_total;
 global $post;
 
 $post_id = get_the_ID();
-$post    = get_post( $post_id );
-$donor   = $post->post_name;
+$donor   = get_post_field( 'post_name', $post_id );
 ?>
-
-<!-- wp:group {"metadata":{"name":"Think Tank Total Donations Received"},"className":"total-donations","layout":{"type":"default"}} -->
-<div class="wp-block-group total-donations">
-	<!-- wp:paragraph -->
-	<p><?php esc_html_e( 'Minimum amount received', 'ttft' ); ?><p>
-	<!-- /wp:paragraph -->
-
-	<!-- wp:heading {"level":4, "className":"dollar-value"} -->
-	<h4 class="dollar-value"><?php echo number_format( $total_donations ); ?></h4>
-	<!-- /wp:heading -->
-</div>
-<!-- /wp:group -->
 
 <!-- wp:group {"metadata":{"name":"Filters"},"className":"data-filters","layout":{"type":"default"}} -->
 <div class="wp-block-group data-filters"><!-- wp:group {"metadata":{"name":"Donation Year"},"layout":{"type":"default"}} -->
