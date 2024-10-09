1bd390684a1c525aebea8b219bbc02f5976fdd99
Pea <pea@misfist.com>
2024-10-09 14:48:54 -0400

    Code formatting

--- patterns/data-table-donor.php
+++ patterns/data-table-donor.php
@@ -19,7 +19,8 @@ $donor   = get_post_field( 'post_name', $post_id );
 ?>
 
 <!-- wp:group {"metadata":{"name":"Filters"},"className":"data-filters","layout":{"type":"default"}} -->
-<div class="wp-block-group data-filters"><!-- wp:group {"metadata":{"name":"Donation Year"},"layout":{"type":"default"}} -->
+<div class="wp-block-group data-filters">
+	<!-- wp:group {"metadata":{"name":"Donation Year"},"layout":{"type":"default"}} -->
 	<div class="wp-block-group">
 		<!-- wp:data-tables/data-filter-donation-year /-->
 	</div>
