0f1da58247bbbf89e932c9f0d78aa68ac16a918d
Pea <pea@misfist.com>
2024-12-13 02:04:41 -0500

    Update search pattern

--- patterns/search-form.php
+++ patterns/search-form.php
@@ -7,18 +7,19 @@
 ?>
 <!-- wp:group {"metadata":{"name":"Search Component"},"className":"search-component","layout":{"type":"default"}} -->
 <div class="wp-block-group search-component">
-    <!-- wp:ttft/search-form {"description":"<?php esc_html_e( 'Examples: Lockheed Martin, Brookings Institution, US Department of Defense', 'ttft' ); ?>","ajax":true} /-->
+    
+    <!-- wp:ttft/search-form {"description":"Examples: Lockheed Martin, Brookings Institution, US Department of Defense","ajax":true} /-->
 
-    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"space-between"}} -->
+    <!-- wp:buttons {"style":{"layout":{"selfStretch":"fixed","flexSize":"100%"}},"layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"}} -->
     <div class="wp-block-buttons">
-        <!-- wp:button {"backgroundColor":"contrast-3","width":50,"metadata":{"name":"Button","bindings":{"__default":{"source":"core/pattern-overrides"}}},"className":"is-style-fill"} -->
-        <div class="wp-block-button has-custom-width wp-block-button__width-50 is-style-fill"><a
-            class="wp-block-button__link has-contrast-3-background-color has-background wp-element-button" href="/think-tanks/">All Think Tanks</a></div>
+        <!-- wp:button {"backgroundColor":"contrast-3","metadata":{"name":"Button","bindings":{"__default":{"source":"core/pattern-overrides"}}},"className":"is-style-fill"} -->
+        <div class="wp-block-button is-style-fill"><a
+            class="wp-block-button__link has-contrast-3-background-color has-background wp-element-button" href="<?php echo esc_url( get_post_type_archive_link( 'think_tank' ) ); ?>"><?php esc_html_e( 'All Think Tanks', 'ttft' ); ?></a></div>
         <!-- /wp:button -->
 
-        <!-- wp:button {"backgroundColor":"contrast-3","width":50,"metadata":{"name":"Button","bindings":{"__default":{"source":"core/pattern-overrides"}}},"className":"is-style-fill"} -->
-        <div class="wp-block-button has-custom-width wp-block-button__width-50 is-style-fill">
-            <a class="wp-block-button__link has-contrast-3-background-color has-background wp-element-button" href="/donors/">All Donors</a></div>
+        <!-- wp:button {"backgroundColor":"contrast-3","metadata":{"name":"Button","bindings":{"__default":{"source":"core/pattern-overrides"}}},"className":"is-style-fill"} -->
+        <div class="wp-block-button is-style-fill">
+            <a class="wp-block-button__link has-contrast-3-background-color has-background wp-element-button" href="<?php echo esc_url( get_post_type_archive_link( 'think_tank' ) ); ?>"><?php esc_html_e( 'All Donors', 'ttft' ); ?></a></div>
         <!-- /wp:button -->
     </div>
     <!-- /wp:buttons -->
