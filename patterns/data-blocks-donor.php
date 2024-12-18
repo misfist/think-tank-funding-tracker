a32202bf279940b270c77d90b79829e99fbaacc1
Pea <pea@misfist.com>
2024-12-17 22:03:55 -0500

    Add logic for Unknown Amt

--- patterns/data-blocks-donor.php
+++ patterns/data-blocks-donor.php
@@ -5,22 +5,37 @@
  * Categories: transparency
  * Inserter: false
  */
-use function Quincy\ttft\get_single_donor_total;
+use function Quincy\ttft\get_single_donor_sums_by_id;
 
-$post_id         = get_the_ID();
-$donor           = get_post_field( 'post_name', $post_id );
-$total_donations = get_single_donor_total( $donor );
-$settngs         = get_option( 'site_settings' );
-$text            = $settngs['donor_total_text'] ?? __( 'Minimum contributions', 'ttft' );
+$post_id          = get_the_ID();
+$settngs          = get_option( 'site_settings' );
+$sums             = get_single_donor_sums_by_id( $post_id );
+$text             = $settngs['donor_total_text'] ?? __( 'Minimum contributions', 'ttft' );
+$undisclosed_text = $settngs['unknown_amount'] ?? __( 'Unknown Amt §', 'ttft' );
+$total            = $sums['amount_calc'];
+$undisclosed      = $sums['undisclosed'];
 ?>
 <!-- wp:group {"metadata":{"name":"Think Tank Total Donations Received"},"className":"total-donations","layout":{"type":"default"}} -->
 <div class="wp-block-group total-donations">
 	<!-- wp:paragraph -->
-	<p><?php echo esc_html( $text ); ?><p>
+	<p><?php echo esc_html( $text ); ?></p>
 	<!-- /wp:paragraph -->
 
-	<!-- wp:heading {"level":4, "className":"dollar-value"} -->
-	<h4 class="dollar-value"><?php echo number_format( $total_donations ); ?></h4>
-	<!-- /wp:heading -->
+	<?php
+	if ( $undisclosed ) :
+		?>
+		<!-- wp:paragraph {"className":"not-disclosed"} -->
+		<p class="not-disclosed"><?php echo esc_html( $undisclosed_text ); ?></p>
+		<!-- /wp:paragraph -->
+		<?php
+	else :
+		?>
+		<!-- wp:heading {"level":4, "className":"dollar-value"} -->
+		<h4 class="dollar-value"><?php echo number_format( $total ); ?></h4>
+		<!-- /wp:heading -->
+		<?php
+	endif;
+	?>
+
 </div>
 <!-- /wp:group -->
--- patterns/data-table-think-tank.php
+++ patterns/data-table-think-tank.php
@@ -11,56 +11,58 @@
  * %VAR4% = donor_type
  * %VAR5% = limit
  */
-use function Quincy\ttft\get_single_think_tank_total;
-use function Quincy\ttft\is_transparent;
-use function Quincy\ttft\is_not_transparent;
+use function Quincy\ttft\generate_think_tank_data_array;
 
 global $post;
-$post_id         = get_the_ID();
-$think_tank      = get_post_field( 'post_name', $post_id );
-$is_limited      = ( get_post_meta( $post_id, 'limited_info', true ) ) ? true : false;
-$is_transparent  = is_transparent( $post_id );
-$non_transparent = is_not_transparent( $post_id );
+$post_id = get_the_ID();
+$data    = generate_think_tank_data_array( $post_id );
+
+if ( ! $data ) {
+	return;
+}
+
+$think_tank                     = $data['think_tank'];
+$is_limited                     = (bool) $data['is_limited'];
+$is_transparent                 = (bool) $data['is_transparent'];
+$is_not_transparent             = (bool) $data['is_not_transparent'];
+$is_undisclosed_or_not_accepted = (bool) $data['is_undisclosed_or_not_accepted'];
+
 $settings        = get_option( 'site_settings' );
-$total_donations = get_single_think_tank_total( $think_tank );
+$total_donations = $data['amount_calc'];
 
-if ( $is_limited ) :
+if ( $is_limited ) {
+	// Displayed for think tanks tagged as having little or no data available
+	$color   = 'accent-10';
+	$content = $settings['think_tank_no_data_text'] ? esc_html( $settings['think_tank_no_data_text'] ) : esc_html__( 'This think tank has not provided data about it\'s donations.', 'ttft' );
+} elseif ( $is_transparent ) {
+	// Displayed for think tanks with a transparency score 4 or higher
+	$color   = 'accent-5';
+	$content = $settings['think_tank_is_transparent_text'] ? esc_html( $settings['think_tank_is_transparent_text'] ) : esc_html__( 'This think tank is transparent.', 'ttft' );
+} elseif ( $is_not_transparent ) {
+	// Displayed for think tanks that didn't accept from 3 donor types, but hava a transparency score 3 or lower
+	$color   = 'accent-6';
+	$content = $settings['think_tank_none_accepted_text'] ? esc_html( $settings['think_tank_none_accepted_text'] ) : esc_html__( 'Donations were not received from these donor types.', 'ttft' );
+}
+
+if ( $is_limited || $is_transparent || $is_not_transparent ) :
+	/**
+	 * Message is displayed in place of table.
+	 */
 	?>
-	<!-- wp:group {"metadata":{"name":"No Data"},"className":"no-data","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"backgroundColor":"var:custom|color|pink-light","layout":{"type":"default"}} -->
-		<div class="wp-block-group has-pink-light-background-color has-background no-data" style="background-color:var(--wp--custom--color--pink-light);padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
-
-			<!-- wp:heading {"level":4} -->
-			<h4><?php echo $settings['think_tank_no_data_text'] ?? esc_html__( 'This think tank has not provided data about it\'s donations.', 'ttft' ); ?></h4>
-			<!-- /wp:heading -->
-
-		</div>
-		<!-- /wp:group -->
-	<?php
-elseif ( $is_transparent ) :
-	?>
-		<!-- wp:group {"metadata":{"name":"Transparent"},"className":"is-transparent","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"backgroundColor":"var:preset|color|accent-5","layout":{"type":"default"}} -->
-		<div class="wp-block-group has-accent-5-background-color has-background is-transparent" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
-
-			<!-- wp:heading {"level":4} -->
-			<h4><?php echo $settings['think_tank_is_transparent_text'] ?? esc_html__( 'This think tank is transparent.', 'ttft' ); ?></h4>
-			<!-- /wp:heading -->
-
-		</div>
-		<!-- /wp:group -->
-	<?php
-elseif ( $non_transparent ) :
-	?>
-	<!-- wp:group {"metadata":{"name":"Not Accepted"},"className":"none-acccepted is-not-transparent","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"backgroundColor":"var:preset|color|accent-6","layout":{"type":"default"}} -->
-	<div class="wp-block-group has-accent-6-background-color has-background none-acccepted is-not-transparent" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
+	<!-- wp:group {"metadata":{"name":"No Data"},"className":"no-data","style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20","left":"var:preset|spacing|20","right":"var:preset|spacing|20"}}},"backgroundColor":"var:custom|color|<?php echo esc_attr( $color ); ?>","layout":{"type":"default"}} -->
+	<div class="wp-block-group has-<?php echo esc_attr( $color ); ?>-background-color has-background no-data" style="padding-top:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
 
 		<!-- wp:heading {"level":4} -->
-		<h4><?php echo $settings['think_tank_none_accepted_text'] ?? esc_html__( 'Donations were not received from these donor types.', 'ttft' ); ?></h4>
+		<h4><?php echo $content; ?></h4>
 		<!-- /wp:heading -->
 
 	</div>
 	<!-- /wp:group -->
 	<?php
 else :
+	/**
+	 * Message containing total donations is display above table.
+	 */
 	?>
 	<!-- wp:group {"metadata":{"name":"Filters"},"className":"data-filters","layout":{"type":"default"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
 	<div class="wp-block-group data-filters" style="margin-top:var(--wp--preset--spacing--40);">
@@ -81,12 +83,28 @@ else :
 	<!-- wp:group {"metadata":{"name":"Think Tank Total Donations Received"},"className":"total-donations","layout":{"type":"default"}} -->
 	<div class="wp-block-group total-donations">
 		<!-- wp:paragraph -->
-		<p><?php echo $settings['think_tank_total_text'] ?? esc_html__( 'Minimum amount received', 'ttft' ); ?></p>
+		<p><?php echo $settings['think_tank_total_text'] ? esc_html( $settings['think_tank_total_text'] ) : esc_html__( 'Minimum amount received', 'ttft' ); ?></p>
 		<!-- /wp:paragraph -->
 
-		<!-- wp:heading {"level":4,"className":"dollar-value"} -->
-		<h4 class="wp-block-heading dollar-value"><?php echo number_format( $total_donations, 0, '.', ',' ); ?></h4>
-		<!-- /wp:heading -->
+		<?php
+		if ( $is_undisclosed_or_not_accepted ) :
+			/**
+			 * If all donor types are either undisclosed or not accepted, display unknown amount.
+			 */
+			?>
+			<!-- wp:heading {"level":4,"className":"not-disclosed"} -->
+			<h4 class="wp-block-heading not-disclosed"><?php echo $settings['unknown_amount'] ?? esc_attr__( 'Unknown Amt', 'ttft' ); ?></h4>
+			<!-- /wp:heading -->
+			<?php
+		else :
+			?>
+			<!-- wp:heading {"level":4,"className":"dollar-value"} -->
+			<h4 class="wp-block-heading dollar-value"><?php echo number_format( $total_donations, 0, '.', ',' ); ?></h4>
+			<!-- /wp:heading -->
+			<?php
+		endif;
+		?>
+		
 	</div>
 	<!-- /wp:group -->
 
@@ -94,6 +112,5 @@ else :
 		"tableType":"single-think-tank",
 		"thinkTank":"<?php echo $think_tank; ?>"
 	} /-->
-	
 	<?php
 endif;
